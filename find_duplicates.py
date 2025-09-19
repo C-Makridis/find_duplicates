8b51647bf1754defe9ecf8ec0f6ebce8aaccd25c
Christos Makridis <C-Makridis@users.noreply.github.com>
2025-09-20 02:37:12 +0300

    add size filter
    add size filter

--- find_duplicates.py
+++ find_duplicates.py
@@ -22,14 +22,17 @@ import argparse
 
 def arguments_Parser():
 	parser = argparse.ArgumentParser(
+		prog = 'find_duplicates',
 		description='Find duplicate files based on MD5 checksums.',
 		epilog=r'''
 Examples:
 %(prog)s /home/user/Downloads /home/user/Pictures/
 %(prog)s "C:\Users\My Name\Documents" "D:\Backup\Documents"
+%(prog)s --greater-than 5 /mnt/backup  # Find duplicates larger than 5MB
 		''',
 		formatter_class=argparse.RawDescriptionHelpFormatter
 	)
+	parser.add_argument('-gt', '--greater-than', type=float, metavar='SIZE_MB', help='Only consider files larger than SIZE_MB megabytes.')
 	parser.add_argument('folders', nargs='+', help='One or more folders to search for duplicates in.')
 	args = parser.parse_args()
 	return args
@@ -38,22 +41,25 @@ def build_size_table(args):
 	sizeTable = dict()
 	filecounter = 0
 	filerr = 0
+	size_threshold_bytes = args.greater_than * 1024 * 1024 if args.greater_than else 0
+	
 	for root in args.folders: # for each folder given
 		for current, dirs, files in os.walk(root): # loop though its contents
 			for fname in files: # and for each file you find
 				fullname = os.path.join(current, fname)
 				filecounter += 1
 				try:
-					myfilesize = os.path.getsize(os.path.join(current, fname))
+					myfilesize = os.path.getsize(fullname)
 					print('.', end='', flush=True) # Progressbar 
 				except (PermissionError, FileNotFoundError, OSError) as e:
 					print('E', end='', flush=True) # Progressbar
 					filerr += 1
 					continue
-				if myfilesize in sizeTable:
-					sizeTable[myfilesize] += (fullname, )
-				else:
-					sizeTable[myfilesize] = (fullname, )
+				if myfilesize > size_threshold_bytes:
+					if myfilesize in sizeTable:
+						sizeTable[myfilesize] += (fullname, )
+					else:
+						sizeTable[myfilesize] = (fullname, )
 	return filecounter, sizeTable, filerr
 
 def hash_suspected_files(sizeTable, filerr):
@@ -103,9 +109,9 @@ def main():
 	print('-'*60)
 	
 	if duplicates == 0:
-		print(f"No duplicate files found among {filecounter} files ({filerr} errors.)")
+		print(f"No duplicate files {' above ' + str(args.greater_than) + ' MB' if args.greater_than else '' } found among {filecounter} files ({filerr} errors.)")
 	else:
-		print(f"{duplicates} duplicate files found among {filecounter} files ({filerr} errors)")
+		print(f"{duplicates} duplicate files {'above ' + str(args.greater_than) + ' MB' if args.greater_than else '' } found among {filecounter} files ({filerr} errors)")
 			
 if __name__ == '__main__':
 	main()
